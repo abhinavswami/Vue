a1aae6d0530dca0455ef420010d7965f9486be47
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-16 22:52:02 -0700

    Update configuration

--- elect.js
+++ elect.js
@@ -1,29 +1,42 @@
 // elect.js
-const { app, BrowserWindow } = require('electron');
+var url = require('url');
+const path = require('path');
+
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 
 let win = null;
 
-app.on('ready', function(){
+app.on('ready', function() {
   // Initialize thw window to our specified dimensions
-  win = new BrowserWindow({ width : 1000, height : 600});
+  win = new BrowserWindow({
+    width: 1000,
+    height: 600
+  });
 
   // Specify entry point to default entry point of vue.js
-  win.loadURL('http://localhost:8080');
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 
   // Remove window once app is closed
-  win.on('closed', function(){
+  win.on('closed', function() {
     win = null;
   });
 });
 
 // create the application window if the window variable is null
 app.on('activate', () => {
-  if(win == null) {
+  if (win == null) {
     createWindow()
   }
 });
 
 // Quit the app once closed
-app.on('window-all-closed', function(){
+app.on('window-all-closed', function() {
   app.quit();
 });
